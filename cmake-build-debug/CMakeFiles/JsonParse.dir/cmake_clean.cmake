b8017ce76c5e4c6d07e5b84a5f665ddc50ed2c86
Hengyiyu"的Macbook Pro <fijiisland@HengyiyudeMacbookdeMacBook-Pro.local>
2020-11-05 21:39:44 +0800

    Initialize Project.

---
+++ cmake-build-debug/CMakeFiles/JsonParse.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JsonParse.dir/leptjson.c.o"
+  "CMakeFiles/JsonParse.dir/test.c.o"
+  "JsonParse"
+  "JsonParse.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/JsonParse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ leptjson.h
@@ -0,0 +1,77 @@
+#ifndef LEPTJSON_H__
+#define LEPTJSON_H__
+
+#include <assert.h>
+#include <stdlib.h>
+
+/*
+ *  Enum lept_type
+ *
+ *  代表的json值类型：
+ *  1. NULL值
+ *  2. Boolean的true与false
+ *  3. 数字类型
+ *  4. 字符串类型
+ *  5. 数组类型
+ *  6. json对象
+ */
+typedef enum {
+    LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER,
+    LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT
+} lept_type;
+
+/*
+ * 匿名Enum
+ *
+ * 分别代表：
+ * 1. 解析成功
+ * 2. 解析了一个空json文本
+ * 3. 非法的json值
+ * 4. json值结尾有非空字符
+ */
+enum {
+    LEPT_PARSE_OK = 0,
+    LEPT_PARSE_EXPECT_VALUE,
+    LEPT_PARSE_INVALID_VALUE,
+    LEPT_PARSE_ROOT_NOT_SINGULAR,
+    LEPT_PARSE_NUMBER_TOO_BIG
+};
+
+/*
+ *  Struct lept_value
+ *
+ *  一个存储json值的结构体
+ */
+typedef struct {
+    double n;
+    lept_type type;
+} lept_value;
+
+/*
+ *  Fuction lept_parse
+ *
+ *  json文本解析入口Api
+ *
+ *  para1: 传入一个json值指针  para2: 传入一个json文本字符串指针
+ */
+int lept_parse(lept_value* v, const char* json);
+
+/*
+ *  Fuction lept_get_type
+ *
+ *  返回一个json值的类型
+ *
+ *  para1: 传入一个json值指针
+ */
+lept_type lept_get_type(const lept_value* v);
+
+/*
+ *  Fuction lept_get_number
+ *
+ *  返回一个json值的double域
+ *
+ *  para1: 传入一个json值指针
+ */
+double lept_get_number(const lept_value* v);
+
+#endif
